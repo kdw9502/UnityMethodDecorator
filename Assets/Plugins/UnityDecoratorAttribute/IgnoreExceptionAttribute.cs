6aa65fcfd11b3dd064f0ec4d76a8480e7362e315
kdw9502 <kdw9502@gmail.com>
2022-09-07 23:33:46 +0900

    Add IgnoreExceptionAttribute

---
+++ Assets/Plugins/UnityDecoratorAttribute/IgnoreExceptionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityDecoratorAttribute
+{
+    public class IgnoreExceptionAttribute
+    {
+        public void CatchException(Exception exception)
+        {
+            Debug.Log($"{exception.Message}");
+        }
+    }
+}
